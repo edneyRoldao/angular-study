9938cc8c257d009d6a25ea1e1d80849d52af8673
edneyRoldao <edneyroldao@gmail.com>
2020-04-09 07:44:52 -0300

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,20 +12,10 @@ export class AppComponent {
   // e1p1
   nome: string = 'ricardo jonas';
 
-  //e2p1
-  email = 'ricardo@mail.com';
-
-  // e3p1
-  sobrenome = 'jonas';
-
   // e1p2
-  atualizar(event: any) {
-    this.nome = event.target.value;
-  }
-
-  // e2p2
-  handleUpdate(value: any) {
-    this.email = value;
+  update(template: any) {
+    console.log(template);
+    this.nome = template.value;
   }
 
 }
