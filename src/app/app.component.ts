f8f4449015199e6d61a731358c07bdb2ac0530e1
edneyRoldao <edneyroldao@gmail.com>
2020-04-13 19:55:42 -0300

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,11 @@ import { Carro } from './carro.interface';
 })
 export class AppComponent {
 
-  //e1p1
   carros: Carro[] = [
     {nome: 'i30', ano: 2010},
     {nome: 'bmw', ano: 2019},
     {nome: 'mercedes', ano: 2020},
+    {nome: 'porche', ano: 2017},
     {nome: 'punto', ano: 2014}
   ];
 
