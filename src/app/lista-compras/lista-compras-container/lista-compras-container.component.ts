eb1ce6ef864927058b7feedbad77473e97fd88c3
edneyRoldao <edneyroldao@gmail.com>
2020-04-15 07:38:53 -0300

    add

---
+++ src/app/lista-compras/lista-compras-container/lista-compras-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lista-compras',
+  templateUrl: 'lista-compras-container.component.html',
+  styleUrls: [
+    'lista-compras-container.component.css'
+  ]
+})
+export class ListaComprasContainerComponent {
+
+}
---
+++ src/app/lista-compras/lista-compras.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { ListaComprasContainerComponent } from './lista-compras-container/lista-compras-container.component'
+
+@NgModule({
+  declarations: [
+    ListaComprasContainerComponent
+  ],
+  exports: [
+    ListaComprasContainerComponent
+  ]
+})
+export class ListaComprasModule {
+
+}
